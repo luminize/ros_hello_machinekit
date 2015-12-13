128a1238f7a2459d7fbf5bc58eb2dfce8e8c8a21
bdebruijn <bdebruijn@luminize.nl>
2015-12-13 19:21:35 +0100

    use float value instead of string for message type

--- scripts/hello_world_publisher.py
+++ scripts/hello_world_publisher.py
@@ -2,10 +2,10 @@
 
 import rospy
 import hal #import the puthon HAL bindings to connect to Machinekit
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32
 
 def talker():
-    pub = rospy.Publisher('hello_pub', String, queue_size=10)
+    pub = rospy.Publisher('hello_pub', Float32, queue_size=10)
     rospy.init_node('hello_world_publisher', anonymous=True)
     r = rospy.Rate(10)
     # Create a new hal userland component
@@ -17,7 +17,7 @@ def talker():
         str = "rospub-inside-HAL.value at %s is %s" % (rospy.get_time(),number)
         rospy.loginfo(str)
         str = "rospub-inside-HAL.value is : %s" % (number)
-        pub.publish(str)
+        pub.publish(number)
         r.sleep()
 
 if __name__ == '__main__':
